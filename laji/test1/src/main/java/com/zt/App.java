468e6906afee63ac9fb21267c6bae8847d9b4d02
wzt <wzt@163.com>
2020-05-15 20:39:10 +0800

    2修改laji

--- laji/test1/src/main/java/com/zt/App.java
+++ laji/test1/src/main/java/com/zt/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+//
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
